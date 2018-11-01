c01b425ab3ef441ad7be95b9d4f1f8d691f0c6e6
Unanoc <leedaniil98@gmail.com>
2018-11-01 14:55:36 +0300

    without PIL

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from PIL import Image
 import time
 import requests
 
-from bs4 import BeautifulSoup
+option = {
+    "format": "png",
+    "crop-h": "1370",
+    "crop-w": "1500",
+    "crop-x": "20",
+    "crop-y": "150",
+    "encoding": "UTF-8"
+    }
+
 
 def get_photo_by_url(url, groupID):
     start = time.time()
-    photo_path = f'{groupID}.jpg'
-    imgkit.from_url(url, photo_path)
+    photo_path = f"{groupID}.png"
+    imgkit.from_url(url, photo_path, options=option)
 
-    img = Image.open(photo_path)
-    area = (20, 150, 1000, 1500)
-    cropped_img = img.crop(area)
-    cropped_img.save(photo_path)
     end = time.time()
     print("{}s".format(end - start))
 
-get_photo_by_url("https://students.bmstu.ru/schedule/62ec2976-a264-11e5-96b1-005056960017", "ИУ6-54Б")
+get_photo_by_url("https://students.bmstu.ru/schedule/62eff510-a264-11e5-bdae-005056960017", "ИУ6-54Б")
 
-
-get_url_by_group("sdf")
